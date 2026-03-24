d82ffcac20085403507c14df4d6e4dacd24724f4
Nishiwaki <plusonefor730@gmail.com>
2026-03-25 06:51:07 +0900

    Finish add-basic-info

---
+++ app/javascript/controllers/index.js
@@ -0,0 +1,4 @@
+import { application } from "controllers/application"
+import EditBasicInfoController from "controllers/edit_basic_info_controller"
+
+application.register("edit-basic-info", EditBasicInfoController)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_21_143629) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_22_205457) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,6 +19,9 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_21_143629) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "department"
+    t.datetime "basic_time", default: "2026-03-22 23:00:00"
+    t.datetime "work_time", default: "2026-03-22 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
